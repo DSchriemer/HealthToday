86d43e9159b71b7f4b8ac83a5ee88ed54e98f80f
David S <david.schriemer7@gmail.com>
2020-04-06 21:23:09 -0700

    Added Prep page

---
+++ app/src/main/java/com/example/volunteer/PrepActivity.java
@@ -0,0 +1,14 @@
+package com.example.volunteer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prep);
+    }
+}
